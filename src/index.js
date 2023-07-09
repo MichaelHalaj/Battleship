097d20d30415bd97c263bc5a3eb5b011b476728a
Michael Halaj <michaelhalaj7@gmail.com>
2023-07-09 09:35:20 -0400

    Test index.js to place ships

--- src/index.js
+++ src/index.js
@@ -6,6 +6,38 @@ const boardSize = Gameboard.standardSize;
 
 const grids = document.querySelectorAll('.grid');
 
+const grid1 = document.getElementById('grid1');
+
+const grid2 = document.getElementById('grid2');
+
+const startBtn = document.querySelector('.start');
+
+const gridItems = document.querySelectorAll('.grid-item');
+
+function stringID(gridNumber, row, col) {
+  return `grid-${gridNumber}-item-${row}-${col}`;
+}
+
+console.log(gridItems.find(item => item.id === 'grid-1-item-0-0'));
+
+function updatePlayerBoardVisual(grid) {
+  
+}
+function gameloop() {
+  const player1 = new Player('Player');
+  const player2 = new Player();
+  player1.gameboard.placeShip('carrier', [0, 0], 'horizontal');
+  player1.gameboard.placeShip('battleship', [4, 3], 'vertical');
+  player1.gameboard.placeShip('destroyer', [6, 9], 'vertical');
+  player1.gameboard.placeShip('submarine', [9, 2], 'horizontal');
+  player1.gameboard.placeShip('patrol boat', [2, 8], 'vertical');
+  console.log(player1.gameboard);
+}
+
+startBtn.addEventListener('click', () => {
+  gameloop();
+});
+
 let gridCount = 1;
 grids.forEach((grid) => {
   grid.setAttribute('style', `display: grid; grid-template-columns: repeat(${boardSize}, 1fr); grid-template-rows: repeat(${boardSize}, 1fr); grid-gap: 2px;`);
@@ -20,14 +52,13 @@ grids.forEach((grid) => {
   gridCount += 1;
 });
 
-const gridItems = document.querySelectorAll('.grid-item');
 gridItems.forEach((item) => {
   item.addEventListener('mouseover', () => {
     const split = item.id.split('-');
     const shipSize = 5;
     for (let i = 0; i < shipSize; i += 1) {
-      const newID = `#${split[0]}-${split[1]}-${split[2]}-${+split[3] + i}-${split[4]}`;
-      const shipItem = document.querySelector(newID);
+      const newID = `${split[0]}-${split[1]}-${split[2]}-${+split[3] + i}-${split[4]}`;
+      const shipItem = document.getElementById(newID);
       // const shipItem = Array.from(gridItems).find(node => node.id == newID);
       // console.log(shipItem);
       shipItem.classList.add('ship-hover');
@@ -37,8 +68,8 @@ gridItems.forEach((item) => {
     const split = item.id.split('-');
     const shipSize = 5;
     for (let i = 0; i < shipSize; i += 1) {
-      const newID = `#${split[0]}-${split[1]}-${split[2]}-${+split[3] + i}-${split[4]}`;
-      const shipItem = document.querySelector(newID);
+      const newID = `${split[0]}-${split[1]}-${split[2]}-${+split[3] + i}-${split[4]}`;
+      const shipItem = document.getElementById(newID);
       // console.log(shipItem);
       shipItem.classList.remove('ship-hover');
     }
